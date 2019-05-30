483b9c41700cd053c31d5ac09e802a81e97cfb0f
Adventure 2019 <caliew26@gmail.com>
2019-05-29 22:06:12 -0400

    adding the query for both id and quantity; getting error, trying to fix

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,46 +13,55 @@ var connection = mysql.createConnection({
     // Your password
     password: "Potter!!13",
     database: "bamazon"
-
-  });
+});
   
-  connection.connect(function(err) {
-    if (err) throw err;    
-    console.log(err);
+connection.connect(function(err) {
+    if (err) throw err;  
     console.log("connected as id " + connection.threadId);
-    connection.end();
-  });
+    // connection.end();
+    allAvail();
+});
 
-  function allAvail(){
-      console.log("allAvail search");
-      var query = "SELECT id, product_name, department_name, price, stock_quantity FROM products";
-      connection.query(query, function(err, resp){
-          if(err) throw err;
-          console.log(err);
-          console.log("\n");
-          for(let i = 0; i < resp.length; i++){
-              console.log(`id: ${resp[i].id} product: ${resp[i].product_name} department: ${resp[i].department_name} price: ${resp[i].price} quantity: ${resp[i].stock_quantity}`);
-          }
-          console.log("\n");
-      })
-  }
+function allAvail(){
+    // console.log("allAvail search");
+    var query = "SELECT id, product_name, department_name, price, stock_quantity FROM products";
+    connection.query(query, function(err, resp){
+        if(err) throw err;
+        console.log("\n");
+        for(let i = 0; i < resp.length; i++){
+            console.log(`id: ${resp[i].id} product: ${resp[i].product_name} department: ${resp[i].department_name} price: ${resp[i].price} quantity: ${resp[i].stock_quantity}`);
+        }
+        console.log("\n");
+        wantToBuy();
+    })
+}
 
-  allAvail();
 
-  function wantToBuy(){
-      inq.prompt([
-          {
-              type: "input",
-              message: "Which item id would you like to buy?",
-              name: "idToBuy"
-          },
-          {
-              type: "number",
-              message: "How many would you like to buy?",
-              name: "amountToBuy"
-          }
-      ]).then(function(answer){
-          console.log("id wanting to buy " + answer.idToBuy);
-          var query = "SELECT id, stock_quantity FROM products WHERE ?"
-      })
-  }
+function wantToBuy(){
+    inq.prompt([
+        {
+            type: "input",
+            message: "Which item id would you like to buy?",
+            name: "idToBuy"
+        },
+        {
+            type: "number",
+            message: "How many of that item would you like to buy?",
+            name: "numberToBuy"
+        }
+    ]).then(function(answer){
+        // console.log("id wanting to buy " + answer.idToBuy);
+        var query = "SELECT id, stock_quantity FROM products WHERE id AND stock_quantity ?";
+        connection.query(query, [answer.idToBuy, answer.numberToBuy], function(error, response){
+            if (error) throw error;
+            console.log("\n");
+            for(let j = 0; j < response.length; j++){
+                console.log(`id: ${response[j].id} stock_quantity: ${response[j].stock_quantity}`);
+            }
+            console.log("\n");
+            connection.end();
+        })
+    })
+}
+
+// wantToBuy();
